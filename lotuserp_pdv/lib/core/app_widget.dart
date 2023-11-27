23a418cfff1bb93289f25df7b09ac0132a602ee7
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-11-27 09:40:58 -0300

    Dependencies added, splash page created

---
+++ lotuserp_pdv/lib/core/app_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lotuserp_pdv/pages/splash.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lotuserp_pdv/lib/pages/splash.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lotuserp_pdv/core/app_colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashScreen> {
+  var _scale = 5.0;
+  var _animationOpacityLogo = 0.0;
+
+  double get _logoAnimationWidth => 800 / _scale;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _animationOpacityLogo = 1.0;
+        _scale = 2;
+      });
+    });
+    Future.delayed(const Duration(seconds: 3), () {
+      //Get.off(() => const AuthScreen());
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          color: CustomColors.customSwatchColor,
+        ),
+        child: Center(
+          child: AnimatedOpacity(
+            duration: const Duration(seconds: 3),
+            curve: Curves.ease,
+            opacity: _animationOpacityLogo,
+            child: AnimatedContainer(
+              width: _logoAnimationWidth,
+              duration: const Duration(seconds: 3),
+              curve: Curves.linearToEaseOut,
+              color: CustomColors.customSwatchColor,
+              child: SizedBox(
+                height: 500,
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
