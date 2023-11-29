884cac84448a88ffd4e20b510d8fc2262732ab76
Deivinson Pereira <lordpoxtaa@gmail.com>
2023-11-29 06:38:53 -0300

    header added

--- lotuserp_pdv/lib/shared/widgets/endpoints_widget.dart
+++ lotuserp_pdv/lib/shared/widgets/endpoints_widget.dart
@@ -1,7 +1,14 @@
 const String _baseUrl = 'http://189.126.106.71/cgi-bin/lotuserp';
 const int companyId = 2;
 
+
+
 class Endpoints {
+
+  static Map<String,String> headerRequisition () {
+    return {'ptoken': 'ed9a811327979c9382ffd6361cfc5013'};
+  }
+
   static String empresa() {
     return '$_baseUrl/pdvmobget01_empresa?pidEmpresa=$companyId';
   }
@@ -13,6 +20,14 @@ class Endpoints {
   }
   static String usuario() {
     return '$_baseUrl/pdvmobget02_usuarios?pidEmpresa=$companyId';
+  }
+
+   static String imagemGrupoUrl(String file) {
+    return '$_baseUrl/getimagem?categoria=GRU&file=$file&result=URL';
+  }
+
+  static String imagemProdutoUrl(String file) {
+    return '$_baseUrl/getimagem?categoria=PRO&file=$file&result=URL';
   }
 
 }
