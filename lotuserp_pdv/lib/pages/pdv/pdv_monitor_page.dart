fc90556a873b7c8f5818ca7d92504146ea6835ee
Deivinson Pereira <lordpoxtaa@gmail.com>
2023-12-10 19:01:35 -0300

    list in pdv monitor adjusted

--- lotuserp_pdv/lib/pages/pdv/pdv_monitor_page.dart
+++ lotuserp_pdv/lib/pages/pdv/pdv_monitor_page.dart
@@ -485,59 +485,59 @@ class _PdvMonitorPageState extends State<PdvMonitorPage> {
 
                                     child: Padding(
                                       padding: const EdgeInsets.all(15.0),
-                                      child: ListView.builder(
-                                        controller: scrollController,
-                                        itemCount: controller.pedidos.length,
-                                        itemBuilder: (context, index) {
-                                          total = formatoBrasileiro.format(
-                                              controller.pedidos[index]
-                                                  ['total']);
-
-                                          var priceFormatado = formatoBrasileiro
-                                              .format(controller.pedidos[index]
-                                                  ['price']);
-
-                                          return Card(
-                                            elevation: 2,
-                                            child: Padding(
-                                              padding: const EdgeInsets.only(
-                                                  right: 8.0),
-                                              child: ListTile(
-                                                contentPadding: EdgeInsets.zero,
-                                                leading: IconButton(
-                                                  onPressed: () {
-                                                    setState(() {
-                                                      controller.removerPedido(
-                                                          index,
-                                                          () => setState);
-                                                    });
-                                                  },
-                                                  icon: const Icon(
-                                                    FontAwesomeIcons.trash,
-                                                    size: 20,
-                                                    color: Color.fromARGB(
-                                                        255, 170, 46, 37),
-                                                  ),
-                                                ),
-                                                title: Text(
-                                                  controller.pedidos[index]
-                                                      ['nomeProduto'],
-                                                  maxLines: 1,
-                                                  overflow:
-                                                      TextOverflow.ellipsis,
-                                                ),
-                                                subtitle: Text(
-                                                    '${controller.pedidos[index]['quantidade']} x R\$$priceFormatado ${controller.pedidos[index]['unidade']}'),
-                                                trailing: Text(
-                                                  ' $total',
-                                                  style: const TextStyle(
-                                                      fontSize: 16),
-                                                ),
-                                              ),
-                                            ),
-                                          );
-                                        },
-                                      ),
+                                      child: Obx(() => ListView.builder(
+                                                                              controller: scrollController,
+                                                                              itemCount: controller.pedidos.length,
+                                                                              itemBuilder: (context, index) {
+                                                                                total = formatoBrasileiro.format(
+                                                                                    controller.pedidos[index]
+                                                                                        ['total']);
+                                      
+                                                                                var priceFormatado = formatoBrasileiro
+                                                                                    .format(controller.pedidos[index]
+                                                                                        ['price']);
+                                      
+                                                                                return Card(
+                                                                                  elevation: 2,
+                                                                                  child: Padding(
+                                                                                    padding: const EdgeInsets.only(
+                                                                                        right: 8.0),
+                                                                                    child: ListTile(
+                                                                                      contentPadding: EdgeInsets.zero,
+                                                                                      leading: IconButton(
+                                                                                        onPressed: () {
+                                                                                          setState(() {
+                                                                                            controller.removerPedido(
+                                                                                                index,
+                                                                                                () => setState);
+                                                                                          });
+                                                                                        },
+                                                                                        icon: const Icon(
+                                                                                          FontAwesomeIcons.trash,
+                                                                                          size: 20,
+                                                                                          color: Color.fromARGB(
+                                                                                              255, 170, 46, 37),
+                                                                                        ),
+                                                                                      ),
+                                                                                      title: Text(
+                                                                                        controller.pedidos[index]
+                                                                                            ['nomeProduto'],
+                                                                                        maxLines: 1,
+                                                                                        overflow:
+                                                                                            TextOverflow.ellipsis,
+                                                                                      ),
+                                                                                      subtitle: Text(
+                                                                                          '${controller.pedidos[index]['quantidade']} x R\$ ${controller.pedidos[index]['unidade']}'),
+                                                                                      trailing: Text(
+                                                                                        ' ',
+                                                                                        style: const TextStyle(
+                                                                                            fontSize: 16),
+                                                                                      ),
+                                                                                    ),
+                                                                                  ),
+                                                                                );
+                                                                              },
+                                                                            )),
                                     ),
                                   ),
                                 ),
