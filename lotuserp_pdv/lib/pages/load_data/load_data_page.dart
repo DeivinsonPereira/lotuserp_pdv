46e8c9672f22489704fa62eae9531607d96817e5
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-12-27 17:58:01 -0300

    adjustment

--- lotuserp_pdv/lib/pages/load_data/load_data_page.dart
+++ lotuserp_pdv/lib/pages/load_data/load_data_page.dart
@@ -14,21 +14,14 @@ class LoadDataPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
       body: Column(children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: IconButton(
-            onPressed: () {
-              Get.offNamed(PagesRoutes.homePageRoute);
-            },
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            ),
-          ),
-        ),
         Expanded(
           child: Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0),
+            padding: const EdgeInsets.only(
+              left: 15.0,
+              right: 15.0,
+              top: 15.0,
+              bottom: 15.0,
+            ),
             child: Container(
               height: size.height,
               width: size.width,
@@ -39,39 +32,70 @@ class LoadDataPage extends StatelessWidget {
                 ),
                 color: Colors.white,
               ),
-              child: Column(children: [
-                Container(
-                  width: size.width,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                    border: Border.all(
-                      width: 1,
-                      color: CustomColors.customSwatchColor[900]!,
-                    ),
-                    color: CustomColors.customSwatchColor[900],
-                  ),
-                  padding: const EdgeInsets.all(15.0),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'CARGA DE DADOS',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold),
+              child: Column(
+                children: [
+                  Container(
+                    width: size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
                       ),
-                      Icon(FontAwesomeIcons.download, size: 35),
-                      
+                      border: Border.all(
+                        width: 1,
+                        color: CustomColors.customSwatchColor[900]!,
+                      ),
+                      color: CustomColors.customSwatchColor[900],
+                    ),
+                    padding: const EdgeInsets.all(15.0),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'CARGA DE DADOS',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Icon(FontAwesomeIcons.download, size: 35),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Container(
+                            width: size.width * 0.2,
+                            height: 639.3,
+                            decoration: BoxDecoration(
+                              color: CustomColors.customSwatchColor[900],
+                              border: Border.all(
+                                  width: 1,
+                                  color: CustomColors.customSwatchColor[900]!),
+                            ),
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    FontAwesomeIcons.wifi,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const Text(
+                                  'Verificar Conexão API',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                ),
+                                
+                              ],
+                            )),
+                      ),
                     ],
                   ),
-                  
-
-                ),
-              ]),
+                ],
+              ),
             ),
           ),
         ),
